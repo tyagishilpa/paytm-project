19fb0914d7f2572c6bc338fcdc06af6886169f25
shilpatyagii <shilpatyagii008@gmail.com>
2025-02-12 08:59:43 +0530

    updated

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -51,49 +51,5 @@ router.post('/transfer',authMiddleware,async(req,res)=>{
     });
 });
 
-// router.post("/transfer", authMiddleware, async (req, res) => {
-//     const { amount, to } = req.body;
-
-//     const account = await Account.findOne({
-//         userId: req.userId
-//     });
-
-//     if (account.balance < amount) {
-//         return res.status(400).json({
-//             message: "Insufficient balance"
-//         })
-//     }
-
-//     const toAccount = await Account.findOne({
-//         userId: to
-//     });
-
-//     if (!toAccount) {
-//         return res.status(400).json({
-//             message: "Invalid account"
-//         })
-//     }
-
-//     await Account.updateOne({
-//         userId: req.userId
-//     }, {
-//         $inc: {
-//             balance: -amount
-//         }
-//     })
-
-//     await Account.updateOne({
-//         userId: to
-//     }, {
-//         $inc: {
-//             balance: amount
-//         }
-//     })
-
-//     res.json({
-//         message: "Transfer successful"
-//     })
-// });
-
 
 module.exports = router;
