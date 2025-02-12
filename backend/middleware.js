8fcc552baf500c7c650dd74609562d6f11ac9f6d
tyagishilpa <shilpatyagii008@gmail.com>
2025-02-12 09:05:27 +0530

    Update middleware.js

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,26 +1,27 @@
-const {JWT_SECRET} = require('./config');
-const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require("./config");
+const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req,res,next) => {
-    const authHeader = req.headers.authorization;
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
 
-    if(!authHeader || !authHeader.startsWith('Bearer')){
-        return res.status(403).json({error: 'Authorization header missing or invalid'});
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (decoded.userId) {
+      req.userId = decoded.userId;
+      next();
     }
-
-    const token = authHeader.split(' ')[1];
-
-    try{
-        const decoded = jwt.verify(token,JWT_SECRET);
-
-        req.userId = decoded.userId;
-
-        next();
-    }catch(err){
-        return res.status(403).json({error: 'Invalid or expired token' });
-    }
+  } catch (error) {
+    res.status(403).json({});
+  }
 };
 
-module.exports={
-    authMiddleware
-}
+module.exports = {
+  authMiddleware,
+};
