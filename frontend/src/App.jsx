1280a216a8d38e4dedf700d5a0e00b683f4c42fc
shilpatyagii <shilpatyagii008@gmail.com>
2025-02-13 09:13:05 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,20 +9,17 @@ import { Dashboard } from "./pages/Dashboard";
 import { SendMoney } from "./pages/SendMoney";
 
 function App() {
-
   return (
-   <>
-   <BrowserRouter>
-   <Routes>
-   <Route path="/" element={<SignUp />} />
-    <Route path="/signup" element={<Signup/>}/>
-    <Route path="/signin" element={<Signin/>}/>
-    <Route path="/dashboard" element={<Dashboard/>}/>
-    <Route path="/send" element={<SendMoney/>}/>
-   </Routes>
-   </BrowserRouter>
-   </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
