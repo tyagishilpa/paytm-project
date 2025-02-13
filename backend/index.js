6ec564634b6e6a8911464a35b0d5e977af2066d6
shilpatyagii <shilpatyagii008@gmail.com>
2025-02-13 11:42:56 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require('cors');
 const rootRouter = require('./routes/index');
+const rootaccountRouter = require("./routes/index")
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1',rootRouter);
+app.use('/api/v1',rootaccountRouter)
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
